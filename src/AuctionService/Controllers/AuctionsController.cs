f77aba4b4fdbe313e23420d1715cfc2cf7967f13
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-01 22:01:01 +0700

    Implement Update Auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -35,6 +36,16 @@ namespace AuctionService.Controllers
                 .FirstOrDefaultAsync(x => x.Id == id);
             if (auction == null) return NotFound();
             return _mapper.Map<AuctionDto>(auction);
+        }
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+        {
+            var auction = _mapper.Map<Auction>(auctionDto);
+            auction.Seller = "test";
+            _context.Auctions.Add(auction);
+            var result = await _context.SaveChangesAsync() > 0;
+            if(!result) return BadRequest("Failed to create auction");
+            return CreatedAtAction(nameof(GetAuctionById), new { id = auction.Id }, _mapper.Map<AuctionDto>(auction));
         }
     }
 }
