c06199fd5c418a934135b443757a25b517dd7b47
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-01 22:24:17 +0700

    Implement Delete Auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -68,5 +68,17 @@ namespace AuctionService.Controllers
             if (result) return Ok();
             return BadRequest("Failed to update auction");
         }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAuction(Guid id)
+        {
+            var auction = await _context.Auctions.FindAsync(id);
+            if (auction == null) return NotFound();
+            // check seller = username
+
+            _context.Auctions.Remove(auction);
+            var result = await _context.SaveChangesAsync() > 0;
+            if (result) return Ok();
+            return BadRequest("Failed to delete auction");
+        }
     }
 }
