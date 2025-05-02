162ccd9234b9b4e33e1815c803275dc31ae32c6a
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-02 10:37:49 +0700

    Apply pagination for the result

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,18 +9,29 @@ namespace SearchService.Controllers
     [ApiController]
     public class SearchController : ControllerBase
     {
+        //searching items with optional search term and pagination.
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string? searchTerm)
+        public async Task<ActionResult<List<Item>>> SearchItems(string? searchTerm, int pageNumber = 1, int pageSize = 4)
         {
-            var query = DB.Find<Item>();
+            // Create a paged search query for the Item collection
+            var query = DB.PagedSearch<Item>();
             query.Sort(x => x.Ascending(a => a.Make));
-            if(!string.IsNullOrEmpty(searchTerm))
+            // If a search term is provided, apply full-text search and sort results by relevance score
+            if (!string.IsNullOrEmpty(searchTerm))
             {
                 query.Match(Search.Full, searchTerm).SortByTextScore();
             }
+            // Apply pagination
+            query.PageNumber(pageNumber);
+            query.PageSize(pageSize);
 
             var result = await query.ExecuteAsync();
-            return result;
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount, // Total number of pages
+                totalCount = result.TotalCount // Total number of matched items
+            });
         }
     }
 }
