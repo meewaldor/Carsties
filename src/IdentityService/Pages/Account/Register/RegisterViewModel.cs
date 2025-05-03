9fd736c3446c74d0190e795ad638b02bb66a1c24
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-03 10:44:23 +0700

    Add register page

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Pages.Account.Register
+{
+    public class RegisterViewModel
+    {
+        public string? ReturnUrl { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+        public string? Button { get; set; }
+    }
+}
