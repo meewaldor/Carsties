0eb23fa3e8be60a79f1438d5fa642431ae06933f
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-07 15:21:18 +0700

    Create a navbar

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Navbar from './nav/Navbar';
 
 export const metadata: Metadata = {
   title: 'Carsties',
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main className='container mx-auto px-5 pt-10'>{children}</main>
+      </body>
     </html>
   );
 }
