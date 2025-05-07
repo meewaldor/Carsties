69027472e677fc4002deadc8d25bb069f6445606
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-07 15:43:26 +0700

    Add Auction Card

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AuctionCard from './AuctionCard';
 
 async function getData() {
   const res = await fetch('http://localhost:6001/search');
@@ -8,5 +9,12 @@ async function getData() {
 
 export default async function Listings() {
   const data = await getData();
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return (
+    <div>
+      {data &&
+        data.results.map((auction: any) => (
+          <AuctionCard auction={auction} key={auction.id} />
+        ))}
+    </div>
+  );
 }
