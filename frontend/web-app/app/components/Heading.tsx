15fe6e8548f30faf007d459e65b58bbbc42a2a0a
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-07 22:08:46 +0700

    add component to display zero result

---
+++ frontend/web-app/app/components/Heading.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type Props = {
+  title: string;
+  subTitle: string;
+  center?: boolean;
+};
+export default function Heading({ subTitle, title, center }: Props) {
+  return (
+    <div className={center ? 'text-center' : 'text-start'}>
+      <div className='text-2xl font-bold'>{title}</div>
+      <div className='font-light text-neutral-500 mt-2'>{subTitle}</div>
+    </div>
+  );
+}
