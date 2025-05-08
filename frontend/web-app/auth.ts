39dd9015ad52f1a3ca5a05cfcef67a5a2920d144
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-08 09:36:01 +0700

    get session detail and add logout function

--- frontend/web-app/auth.ts
+++ frontend/web-app/auth.ts
@@ -11,9 +11,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     id: 'id-server',
     clientId: "nextApp",
     clientSecret: "secret",
-    issuer: "http://localhost:5001",
+    issuer: "http://localhost:5000",
     authorization: {params: {scope: 'openid profile auctionApp'}},
     idToken: true
-  } as OIDCConfig<Profile>),
+  } as OIDCConfig<Omit<Profile, 'username'>>),
 ],
+callbacks: {
+  async jwt({token, profile}) {
+    if(profile) {
+      token.username = profile.username
+    }
+    return token;
+  },
+  async session({session, token}) {
+    if(token) {
+      session.user.username = token.username
+    }
+    return session;
+  }
+}
 })
