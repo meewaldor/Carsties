d03dea4d91d84911cd7af187b0dcf48471783d5d
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-08 20:37:30 +0700

    Add models to the Bid Service

---
+++ src/BiddingService/Models/Bid.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Entities;
+
+namespace BiddingService.Models
+{
+    public class Bid : Entity
+    {
+        public string AuctionId { get; set; }
+        public string Bidder { get; set; }
+        public int Amount { get; set; }
+        public DateTime BidTime { get; set; } = DateTime.UtcNow;
+        public BidStatus BisStatus { get; set; } 
+
+    }
+}
