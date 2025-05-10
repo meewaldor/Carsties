5406f7656bd43126ebaef0292cd7caf46d6a25a4
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-10 11:33:28 +0700

    Add middleware for handling error

---
+++ src/GatewayService/Errors/ApiErrorResponse.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GatewayService.Errors
+{
+    public class ApiErrorResponse(int statusCode, string message, string? details = null)
+    {
+        public int StatusCode { get; set; } = statusCode;
+        public string Message { get; set; } = message;
+        public string? Details { get; set; } = details;
+    }
+}
---
+++ src/GatewayService/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using GatewayService.Errors;
+using System.Net;
+using System.Text.Json;
+
+namespace GatewayService.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly IHostEnvironment _env;
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(IHostEnvironment env, RequestDelegate next)
+        {
+            _env = env;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                // Pass the request to the next middleware in the pipeline
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex, _env);
+            }
+        }
+
+        // Method to handle exceptions and format the error response
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex, IHostEnvironment env)
+        {
+            context.Response.ContentType = "application/json";
+
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            if (ex is ArgumentException || ex is InvalidOperationException)
+            {
+                statusCode = (int)HttpStatusCode.BadRequest;
+            }
+            context.Response.StatusCode = statusCode;
+
+            // Show stack trace in development mode for debugging purposes
+            var response = env.IsDevelopment()
+                ? new ApiErrorResponse(context.Response.StatusCode, ex.Message, ex.StackTrace)
+                : new ApiErrorResponse(context.Response.StatusCode, ex.Message, "Internal server error");
+
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            var json = JsonSerializer.Serialize(response, options);
+
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
