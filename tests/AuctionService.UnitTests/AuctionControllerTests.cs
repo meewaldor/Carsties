d97abf41af27811878669489e4772e9ece70e7e2
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-13 23:19:16 +0700

    Unit testing for get method

--- tests/AuctionService.UnitTests/AuctionControllerTests.cs
+++ tests/AuctionService.UnitTests/AuctionControllerTests.cs
@@ -1,9 +1,11 @@
 ﻿using AuctionService.Controllers;
 using AuctionService.Data;
+using AuctionService.DTOs;
 using AuctionService.RequestHelpers;
 using AutoFixture;
 using AutoMapper;
 using MassTransit;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 
@@ -29,6 +31,52 @@ namespace AuctionService.UnitTests
 
             _mapper = new Mapper(mockMapper);
             _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+        }
+
+        [Fact]
+        public async Task GetAuctions_WithNoParams_Returns10Auctions()
+        {
+            // arrange
+            var auctions = _fixture.CreateMany<AuctionDto>(10).ToList();
+            _auctionRepo.Setup(repo => repo.GetAuctionsAsync(null))
+                .ReturnsAsync(auctions);
+
+            // act
+            var result = await _controller.GetAllAuctions(null);
+
+            // assert
+            Assert.Equal(10, result.Value.Count);
+            Assert.IsType<ActionResult<List<AuctionDto>>>(result);
+        }
+
+        [Fact]
+        public async Task GetAuctionById_WithValid_ReturnsAuction()
+        {
+            // arrange
+            var auction = _fixture.Create<AuctionDto>();
+            _auctionRepo.Setup(repo => repo.GetAuctionByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(auction);
+
+            // act
+            var result = await _controller.GetAuctionById(auction.Id);
+
+            // assert
+            Assert.Equal(auction.Make, result.Value.Make);
+            Assert.IsType<ActionResult<AuctionDto>>(result);
+        }
+
+        [Fact]
+        public async Task GetAuctionById_WithInvalidGuid_ReturnsNotFound()
+        {
+            // arrange
+            _auctionRepo.Setup(repo => repo.GetAuctionByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(value: null);
+
+            // act
+            var result = await _controller.GetAuctionById(Guid.NewGuid());
+
+            // assert
+            Assert.IsType<NotFoundResult>(result.Result);
         }
     }
 }
