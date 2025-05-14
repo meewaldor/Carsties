83273bf541efc6732f0ccf38724361557115cfeb
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-14 09:39:12 +0700

    Set up a cusstom web application factory for integration testing

--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -1,9 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using AuctionService.Data;
+using AuctionService.IntegrationTests.Utils;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel;
+using Testcontainers.PostgreSql;
 
 namespace AuctionService.IntegrationTests.Fixtures
 {
-    public class CustomWebAppFactory : WebApplicationFactory<Program>
+    // Custom WebApplicationFactory to override app configuration for integration testing
+    public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
     {
+        // Create a PostgreSQL container instance using Testcontainers
+        private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+        public async Task InitializeAsync()
+        {
+            await _postgreSqlContainer.StartAsync();
+        }
+        // Configures the test web host environment and services
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                
+                services.RemoveDbContext<AuctionDbContext>();
+                
+                // Register DbContext using the PostgreSQL test container's connection string
+                services.AddDbContext<AuctionDbContext>(options =>
+                {
+                    options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+                });
 
+                // Add MassTransit test harness to test message consumers without needing a real message broker
+                services.AddMassTransitTestHarness();
+
+                services.EnsureCreated<AuctionDbContext>();
+
+            });
+        }
+
+        //Disposes of the PostgreSQL container.
+       Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
     }
 }
---
+++ tests/AuctionService.IntegrationTests/Utils/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿
+
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionService.IntegrationTests.Utils
+{
+    public static class ServiceCollectionExtensions
+    {
+        // Remove the existing DbContext configuration
+        public static void RemoveDbContext<T>(this IServiceCollection services)
+        {
+            var descriptor = services.SingleOrDefault(d =>
+            d.ServiceType == typeof(DbContextOptions<AuctionDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+        }
+
+        // Build service provider and apply any EF Core migrations to prepare the schema
+        public static void EnsureCreated<T>(this IServiceCollection services)
+        {           
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<AuctionDbContext>();
+
+            db.Database.Migrate();
+            DbHelper.InitDbForTests(db);
+        }
+    }
+}
