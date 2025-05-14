2d64009df140ff7aa4d906192896d9e0ac077578
Lam My <157933381+meewaldor@users.noreply.github.com>
2025-05-14 11:22:58 +0700

    Use collection fixxtures to share DB across the test classes

--- tests/AuctionService.IntegrationTests/AuctionControllerTests.cs
+++ tests/AuctionService.IntegrationTests/AuctionControllerTests.cs
@@ -9,7 +9,8 @@ using System.Net.Http.Json;
 
 namespace AuctionService.IntegrationTests
 {
-    public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
+    [Collection("Shared collection")]
+    public class AuctionControllerTests: IAsyncLifetime
     {
         private readonly CustomWebAppFactory _factory;
         private readonly HttpClient _httpClient;
